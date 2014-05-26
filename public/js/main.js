5a57faf4124282e53c58efacc2e80351a833b2c9
Roshan Vid <roshanvid@gmail.com>
2014-05-26 15:33:37 -0700

    Refactored more code

--- public/js/main.js
+++ public/js/main.js
@@ -7,16 +7,32 @@ $(function() {
         fb_main: null,
         fb_data: null,
         pageThreads: null,
+        pdfDoc: null,
         videoMsgs: {},
         recordRTC_Video: null,
         recordRTC_Audio: null,
+        play_video: function(video_id) {
+            var source = document.createElement("source");
+            var vid = rwm.videoMsgs[video_id];
+            source.src = URL.createObjectURL(base64_to_blob(vid.videoBlob));
+            source.type = "video/webm";
+            $("#video").empty();
+            $("#video").append(source);
+            $("#video").get(0).play();
+
+            var source = document.createElement("source");
+            source.src = URL.createObjectURL(base64_to_blob(vid.audioBlob));
+            source.type = "audio/ogg";
+            $("#audio").empty();
+            $("#audio").append(source);
+            $("#audio").get(0).play();
+        },
         getVideo: function(linkID){
             fb_data.child(linkID).once("value", function(snapshot){
                 this.videoMsgs[linkID] = snapshot.val();
             });
         },
         renderMsg: function(threadID, msgID){
-            console.log(threadID);
             var msg = this.pageThreads[threadID].messages[msgID];
             FB.api('/'+msg.userID+'/picture', function(response){
                     if (!response.error) {
@@ -26,13 +42,12 @@ $(function() {
                         //Assign appropriate click handlers/UI
                         if(msg.type === "video"){
                             rwm.fb_data.child(msg.linkID).once("value", function(snapshot){
-                                console.log(snapshot.val());
                                 rwm.videoMsgs[msg.linkID] = snapshot.val();
                             });
-                            elem.click(function(){
-                                play_video(msg.linkID);
-                            })
                             Tipped.create(elem.get(), {inline: 'video_overlay'});
+                            elem.click(function(){
+                                rwm.play_video(msg.linkID);
+                            })
                         } else if(msg.type === "text"){
                             Tipped.create(elem.get(), msg.text);
                         }
@@ -43,7 +58,6 @@ $(function() {
                     }
             });
 
-            console.log(msg);
         },
         renderAnnotations: function(){
             for(x in this.pageThreads){
@@ -65,7 +79,6 @@ $(function() {
         reloadAnnotations: function(){
             this.fb_main.child(this.bookID).child(this.pageNum).once('value', function(snapshot){
                 rwm.pageThreads = snapshot.val();         
-                console.log(rwm.pageThreads);
                 $("#playback_bar").empty();
                 rwm.renderAnnotations();
             }); 
@@ -84,10 +97,10 @@ $(function() {
                 },
                 messages:{}
             }
-            return this.fb_main.child(this.bookID).child(pageNum).push(stuff_to_upload).name();
+            return this.fb_main.child(this.bookID).child(rwm.pageNum).push(stuff_to_upload).name();
         },
         append_msg_to_thread: function(threadID, data){
-            this.fb_main.child(this.bookID).child(pageNum).child(threadID).child("messages").push(data);
+            this.fb_main.child(this.bookID).child(rwm.pageNum).child(threadID).child("messages").push(data);
         },
         get_text_message_and_upload: function(threadID){
             var res = prompt("Please enter comment:");
@@ -111,7 +124,6 @@ $(function() {
                         blob_to_base64(rwm.recordRTC_Video.getBlob(), function(base64blob) {
                             stuff_to_upload_fbdata.videoBlob = base64blob;
                             var res = rwm.fb_data.push(stuff_to_upload_fbdata);
-                            console.log(res.name());
                             stuff_to_upload_fbmain.linkID = res.name();
                             rwm.append_msg_to_thread(threadID, stuff_to_upload_fbmain);
                         });
@@ -131,7 +143,7 @@ $(function() {
                 var text_upload = {};
                 text_upload.y = e.pageY;
                 text_upload.text = prompt("Please enter text annotation");
-                fb_session.child('' + window.pageNum).child("text").push(text_upload);
+                fb_session.child('' + rwm.pageNum).child("text").push(text_upload);
             });
             
             $("#recordVideo").mousedown(function(e){
@@ -150,8 +162,57 @@ $(function() {
             });
             Tipped.create('#pdfarea', {inline: "toolbar", showOn: 'click', behavior: 'sticky', hideOn: {element: 'click', tooltip: 'click'}});
             Tipped.create("#recordVideo", {inline: 'webcam_stream'})
+
+            document.getElementById('prev').addEventListener('click', rwm.goPrevious);
+            document.getElementById('next').addEventListener('click', rwm.goNext);
         },
 
+        initPdf: function(){
+            var url = "http://s3-us-west-2.amazonaws.com/readwithme/" + global.book_id + ".pdf";
+            PDFJS.disableWorker = true;
+            PDFJS.getDocument(url).then(function getPdfHelloWorld(_pdfDoc) {
+                rwm.pdfDoc = _pdfDoc;
+                rwm.renderPage(rwm.pageNum);
+            });
+        },
+
+
+        goPrevious: function() {
+            if (rwm.pageNum <= 1)
+                return;
+            rwm.pageNum--;
+            rwm.renderPage(rwm.pageNum);
+            rwm.reloadAnnotations();
+            window.scrollTo(0, 0);
+        },
+
+        goNext: function() {
+            if (rwm.pageNum >= rwm.pdfDoc.numPages)
+                return;
+            rwm.pageNum++;
+            rwm.renderPage(rwm.pageNum);
+            rwm.reloadAnnotations();
+            window.scrollTo(0, 0)
+        },
+
+        renderPage: function(num) {
+            var scale = 1.3,
+                canvas = document.getElementById('pdfarea'),
+                ctx = canvas.getContext('2d');
+            this.pdfDoc.getPage(num).then(function(page) {
+                var viewport = page.getViewport(scale);
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                var renderContext = {
+                    canvasContext: ctx,
+                    viewport: viewport
+                };
+                page.render(renderContext);
+            });
+            document.getElementById('page_num').textContent = rwm.pageNum;
+            document.getElementById('page_count').textContent = rwm.pdfDoc.numPages;
+        },
         initFacebook: function() {
             window.fbAsyncInit = function() {
                 FB.init({
@@ -244,188 +305,13 @@ $(function() {
             this.initFirebase();
             this.bookID = global.book_id;
             this.initWebcam();
+            this.initPdf();
             this.reloadAnnotations();
             this.bindUIActions();
         }
     }
 
     rwm.init();
-
-    $(document).ready(function() {
-        setup_pdf();
-        setup_webcam();
-    });
-
-    play_video = function(video_id) {
-        var source = document.createElement("source");
-        var vid = rwm.videoMsgs[video_id];
-        source.src = URL.createObjectURL(base64_to_blob(vid.videoBlob));
-        source.type = "video/webm";
-        $("#video").empty();
-        $("#video").append(source);
-        // var offset = 250 + window.scrollY;
-        // $("#video_overlay").css({
-        //     "top": offset
-        // });
-        // $("#video_overlay").addClass("show");
-        $("#video").get(0).play();
-
-        var source = document.createElement("source");
-        source.src = URL.createObjectURL(base64_to_blob(vid.audioBlob));
-        source.type = "audio/ogg";
-        $("#audio").empty();
-        $("#audio").append(source);
-        $("#audio").get(0).play();
-    }
-
-    function reload_videos_on_page(pNum) {
-        $("#playback_bar").empty();
-        fb_session.child('' + pageNum).child("text").on('value', function(snapshot) {
-            text_msgs = snapshot.val();
-            for (key in text_msgs) {
-                (function(key) {
-                    var text_obj = text_msgs[key];
-                    var elem = $('<div></div>').attr('id', key).addClass('textHead').css({
-                        "top": text_obj.y,
-                        "position": "absolute"
-                    });
-                    var text_dom_elem = document.createTextNode(text_obj.text);
-                    elem.append(text_dom_elem);
-
-                    $("#playback_bar").append(elem);
-
-                })(key);
-            }
-        });
-        fb_session.child('' + pageNum).child("video").on('value', function(snapshot) {
-            video_msgs = {};
-
-            var video_dump = snapshot.val();
-            for (key in video_dump) {
-                video_msgs[key] = video_dump[key];
-                for (res in video_dump[key].responses) {
-                    video_msgs[res] = video_dump[key].responses[res];
-                }
-            }
-
-            function appendToRootUlDiv(key, root_key) {
-                var vid = video_msgs[key];
-                console.log(vid);
-                if (!vid.userid) {
-                    vid.userid = "10202137132692979";
-                }
-                FB.api('/' + vid.userid + '/picture', function(response) {
-                    if (!response.error) {
-                        $("#root_" + root_key).append($('<li><div><img class="msg-icon" src="' + response.data.url + '"></img></div></li>')
-                            .attr('id', key)
-                            .addClass('videoHead')
-                            .click(function() {
-                                play_video(key);
-                            }));
-                    } else {
-                        $("#root_" + root_key).append($('<li><div><img class="msg-icon" src="/img/letter-closed.png"></img></div></li>')
-                            .attr('id', key)
-                            .addClass('videoHead')
-                            .css({
-                                "top": vid.y,
-                                "position": "absolute"
-                            })
-                            .click(function() {
-                                play_video(key);
-                            }));
-                    }
-                });
-            }
-            for (key in video_dump) {
-                //Closures FTW!
-                (function(key) {
-                    var elem = $('<div/>').append(
-                        $('<ul id = root_' + key + '> </ul>'))
-                        .css({
-                            "top": video_dump[key].y,
-                            "position": "absolute"
-                        });
-                    $("#playback_bar").append(elem);
-                    appendToRootUlDiv(key, key)
-                    var resp = video_dump[key].responses;
-                    console.log(resp);
-                    for (key2 in resp) {
-                        console.log(key2);
-                        appendToRootUlDiv(key2, key);
-                    }
-                    var rec_button = $('<img src="/img/record.png"></img>');
-                    rec_button.mousedown(function(e) {
-                        console.log("Started recording response");
-                        $("#webcam_stream").css("visibility", "visible");
-                        $("#webcam_stream").css({
-                            "top": window.scrollY
-                        });
-                        rwm.record_audio_and_video();
-                    });
-
-                    rec_button.mouseup(function(e) {
-                        console.log("Finished recording response");
-                        $("#webcam_stream").css("visibility", "hidden");
-                        rwm.stop_recording_and_upload_response("-JNrca2jtRY1YsIYe_FP");
-                    });
-                    $("#root_" + key).append(rec_button);
-
-                })(key);
-            }
-        });
-    }
-
-    function setup_pdf() {
-        var url = "http://s3-us-west-2.amazonaws.com/readwithme/" + global.book_id + ".pdf";
-        PDFJS.disableWorker = true;
-        window.pageNum = 1;
-        var pdfDoc = null,
-            scale = 1.3,
-            canvas = document.getElementById('pdfarea'),
-            ctx = canvas.getContext('2d');
-
-        function renderPage(num) {
-            pdfDoc.getPage(num).then(function(page) {
-                var viewport = page.getViewport(scale);
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                var renderContext = {
-                    canvasContext: ctx,
-                    viewport: viewport
-                };
-                page.render(renderContext);
-            });
-            document.getElementById('page_num').textContent = pageNum;
-            document.getElementById('page_count').textContent = pdfDoc.numPages;
-            document.getElementById('prev').addEventListener('click', goPrevious);
-            document.getElementById('next').addEventListener('click', goNext);
-        }
-
-        function goPrevious() {
-            if (pageNum <= 1)
-                return;
-            pageNum--;
-            renderPage(pageNum);
-            reload_videos_on_page(pageNum);
-            window.scrollTo(0, 0);
-        }
-
-        function goNext() {
-            if (pageNum >= pdfDoc.numPages)
-                return;
-            pageNum++;
-            renderPage(pageNum);
-            reload_videos_on_page(pageNum);
-            window.scrollTo(0, 0)
-        }
-
-        PDFJS.getDocument(url).then(function getPdfHelloWorld(_pdfDoc) {
-            pdfDoc = _pdfDoc
-            renderPage(pageNum);
-        });
-        // reload_videos_on_page(pageNum);
-    }
 
     var blob_to_base64 = function(blob, callback) {
         var reader = new FileReader();
@@ -448,24 +334,4 @@ $(function() {
         var blob = new Blob([view]);
         return blob;
     };
-
-    function setup_webcam() {
-        function stop_recording_and_upload(yPos) {
-            var stuff_to_upload = {}
-            stuff_to_upload.y = yPos;
-            stuff_to_upload.userid = me.id;
-            rwm.recordRTC_Audio.stopRecording(function(audioURL) {
-                blob_to_base64(rwm.recordRTC_Audio.getBlob(), function(base64blob) {
-                    stuff_to_upload.audioBlob = base64blob;
-                    rwm.recordRTC_Video.stopRecording(function(videoURL) {
-                        blob_to_base64(rwm.recordRTC_Video.getBlob(), function(base64blob) {
-                            stuff_to_upload.videoBlob = base64blob;
-                            fb_session.child('' + window.pageNum).child("video").push(stuff_to_upload);
-                        });
-                    });
-                });
-            });
-        }
-        // setup audio recording
-    }
 });
