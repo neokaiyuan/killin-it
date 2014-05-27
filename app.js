b50732d56fadb482f656b5dc88bd06f2632cf7e9
Roshan Vid <roshanvid@gmail.com>
2014-05-26 18:27:44 -0700

    Got basic pdf upload working

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
+var crypto = require('crypto');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var shortid = require('shortid');
 var app = express();
 
 // view engine setup
@@ -17,13 +19,43 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
+console.log(S3_BUCKET);
 app.get('/', function(req, res){
-    // res.render('index', {title: "ReadWithMe" ,book_id: null });
-    res.redirect('/book/746829');
+    res.render('home');
+});
+
+app.get('/sign_s3', function(req, res){
+    // var object_name = req.query.s3_object_name;
+    var object_name = shortid.generate() + ".pdf";
+    var mime_type = req.query.s3_object_type;
+
+    var now = new Date();
+    var expires = Math.ceil((now.getTime() + 10000)/1000); // 10 seconds from now
+    var amz_headers = "x-amz-acl:public-read";
+
+    var put_request = "PUT\n\n"+mime_type+"\n"+expires+"\n"+amz_headers+"\n/"+S3_BUCKET+"/"+object_name;
+
+    console.log(put_request);
+    var signature = crypto.createHmac('sha1', AWS_SECRET_KEY).update(put_request).digest('base64');
+    signature = encodeURIComponent(signature.trim());
+    signature = signature.replace('%2B','+');
+
+    var url = 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+object_name;
+
+    var credentials = {
+        signed_request: url+"?AWSAccessKeyId="+AWS_ACCESS_KEY+"&Expires="+expires+"&Signature="+signature,
+    url: url
+    };
+    console.log(credentials);
+    res.write(JSON.stringify(credentials));
+    res.end();
 });
 
 app.get('/book/:id', function(req,res){
+    console.log(req.params.id);
     res.render('index', {title: "ReadWithMe", book_id:req.params.id});
 });
 
---
+++ public/js/home.js
@@ -0,0 +1,18 @@
+function s3_upload(){
+    var s3upload = new S3Upload({
+        file_dom_selector: 'files',
+        s3_sign_put_url: '/sign_s3',
+        onProgress: function(percent, message) {
+            $('#status').html('Upload progress: ' + percent + '% ' + message);
+        },
+        onFinishS3Put: function(public_url) {
+            $('#status').html('Upload completed. Uploaded to: '+ public_url);
+        },
+        onError: function(status) {
+            $('#status').html('Upload error: ' + status);
+        }
+    });
+
+}$(function() {
+    $('#files').on("change", s3_upload);
+});
